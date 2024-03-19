a2c0c3f39c015a7872ad31727d2b3d53aa3519fa
ksk <kskw08420@gmail.com>
2024-03-19 17:10:05 +0900

    init: initialized project 프로젝트 초기화

---
+++ src/main/java/com/ksk/footballpoter/FootballpoterApplication.java
@@ -0,0 +1,13 @@
+package com.ksk.footballpoter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballpoterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballpoterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksk/footballpoter/FootballpoterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksk.footballpoter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballpoterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
